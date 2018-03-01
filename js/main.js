661eced404c19edab2a2b307a1052dc91eb9bfb6
ruthmz <rmatosfer@gmail.com>
2018-03-01 10:35:02 -0500

    arreglando estilos

--- js/main.js
+++ js/main.js
@@ -13,15 +13,20 @@ $(document).ready(function() {
   console.log(miraflores);
   var button = '<p type="button" class="btn btn-primary btn-style" data-toggle="modal" data-target="#miModal">Hola Mundo</p>';
   // Cuando selecciona Miraflores
+  $('.title-font').click(function(event){
+    $('.suggested').removeClass('display');
+    $('.image-background1').addClass('hide');
+    $('.image-background2').addClass('hide');
+  })
   $('.miraflores').click(function(event) {
     $('.suggested').addClass('display');
-    $('.image-background1').addClass('miraflores-1').html('<p type="button" class="btn btn-primary btn-style" data-toggle="modal" data-target="#miModal" id= "myBtn"></p>');
+    $('.image-background1').addClass('miraflores-1 border').html('<p type="button" class="btn btn-primary btn-style" data-toggle="modal" data-target="#miModal" id= "myBtn"></p>');
     $('.btn-style').append(miraflores[0]);
     $('.image-background1').removeClass('isidro-1');
     $('.image-background2').removeClass('isidro-2');
     $('.image-background1').removeClass('surco-1');
     $('.image-background2').removeClass('surco-2');
-    $('.image-background2').addClass('miraflores-2').html('<p type="button" class="btn btn-primary btn-style2" data-toggle="modal" data-target="#miModal2" id= "myBtn2"></p>');
+    $('.image-background2').addClass('miraflores-2 border').html('<p type="button" class="btn btn-primary btn-style2" data-toggle="modal" data-target="#miModal2" id= "myBtn2"></p>');
     $('.btn-style2').append(miraflores[1]);
     // Agregando modal cuando se seleccione la opción de miraflores
     $('.image-background1').on('click', function(event) {
@@ -40,13 +45,13 @@ $(document).ready(function() {
   // Cuando selecciona San Isidro
   $('.san-isidro').click(function(event) {
     $('.suggested').addClass('display');
-    $('.image-background1').addClass('isidro-1').html('<p type="button" class="btn btn-primary btn-style" data-toggle="modal" data-target="#miModal" id= "myBtn"></p>');
+    $('.image-background1').addClass('isidro-1 border').html('<p type="button" class="btn btn-primary btn-style " data-toggle="modal" data-target="#miModal" id= "myBtn"></p>');
     $('.btn-style').append(isidro[0]);
     $('.image-background1').removeClass('miraflores-1');
     $('.image-background2').removeClass('miraflores-2');
     $('.image-background1').removeClass('surco-1');
     $('.image-background2').removeClass('surco-2');
-    $('.image-background2').addClass('isidro-2').html('<p type="button" class="btn btn-primary btn-style2" data-toggle="modal" data-target="#miModal2" id= "myBtn2"></p>');
+    $('.image-background2').addClass('isidro-2 border').html('<p type="button" class="btn btn-primary btn-style2" data-toggle="modal" data-target="#miModal2" id= "myBtn2"></p>');
     $('.btn-style2').append(isidro[1]);
     // Agregando modal cuando se seleccione la opción de San Isidro
 
@@ -66,13 +71,13 @@ $(document).ready(function() {
   // Cuando selecciona Surco
   $('.surco').click(function(event) {
     $('.suggested').addClass('display');
-    $('.image-background1').addClass('surco-1').html('<p type="button" class="btn btn-primary btn-style" data-toggle="modal" data-target="#miModal" id= "myBtn"></p>');
+    $('.image-background1').addClass('surco-1 border').html('<p type="button" class="btn btn-primary btn-style" data-toggle="modal" data-target="#miModal" id= "myBtn"></p>');
     $('.btn-style').append(surco[0]);
     $('.image-background1').removeClass('miraflores-1');
     $('.image-background2').removeClass('miraflores-2');
     $('.image-background1').removeClass('isidro-1');
     $('.image-background2').removeClass('isidro-2');
-    $('.image-background2').addClass('surco-2').html('<p type="button" class="btn btn-primary btn-style2" data-toggle="modal" data-target="#miModal2" id= "myBtn2"></p>');
+    $('.image-background2').addClass('surco-2 border').html('<p type="button" class="btn btn-primary btn-style2" data-toggle="modal" data-target="#miModal2" id= "myBtn2"></p>');
     $('.btn-style2').append(surco[1]);
     // Agregando modal cuando se seleccione la opción de Surco
     $('.image-background1').on('click', function(event) {
